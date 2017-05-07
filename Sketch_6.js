8153eeb32883d49f359f8e21a151e68fc3d5a0fb
ABrumley88 <brumley@cca.edu>
2017-05-07 15:27:41 -0700

    Create Sketch_6.js

---
+++ Sketch_6.js
@@ -0,0 +1,29 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var a= 200
+var b= 200
+var c= 3
+
+function draw() { 
+	background(255)
+
+	ellipse(a,b,50,50)
+	
+	if(a>400) {
+		c=-3
+} else 
+	
+	if(a<0) {
+	c=3
+}
+	a=a+c
+	
+}
+
+function mousePressed() {
+  a=mouseX
+	b=mouseY
+
+}
