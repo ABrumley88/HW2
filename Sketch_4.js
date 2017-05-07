bf7527f2d4f8ebd86f3f724135aebf0a5deba288
ABrumley88 <brumley@cca.edu>
2017-05-07 15:25:05 -0700

    Create Sketch_4.js

---
+++ Sketch_4.js
@@ -0,0 +1,19 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+var a= 200
+var b= 200
+var c= 3
+ 
+
+function draw() { 
+	background(255)
+	
+	ellipse(a,b,50,50)
+	
+	if(a>400) {
+		c=-3
+
+  
+  } a=a+c
+}
